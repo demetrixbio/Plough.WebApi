eac6211a903fec5a45fa324e819039c7d3ff709e
Jimmy Byrd <jimmybyrd@gmail.com>
2022-07-21 10:32:24 -0400

    MNT: Build script allows simple releasing

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ On Unix:
 ```shell
 ./fake.sh build
 ```
+
+
+## Releasing
+
+### On Windows:
+
+1. Open `release.ps1` and place your [NUGET_TOKEN](https://docs.microsoft.com/en-us/nuget/nuget-org/publish-a-package#create-api-keys=) and [GITHUB_TOKEN](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token).  This file is in `.gitignore` so it should be difficult to push your keys to github.
+2. Run `.\release.ps1`
+
+### On Unix
+
+```bash
+NUGET_TOKEN=myNugetToken GITHUB_TOKEN=myGithubToken ./fake.sh build -t Release
+```
--- build.fsx
+++ build.fsx
@@ -6,11 +6,22 @@
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
+open Fake.DotNet
+open Fake.Tools
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+open Fake.Api
+open Fake.BuildServer
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
 let rootDir = __SOURCE_DIRECTORY__ 
+let gitOwner = "demetrixbio"
+let gitRepoName = "Plough.WebApi"
+
 
 let sln = rootDir </> "Plough.WebApi.sln"
 
@@ -20,6 +31,17 @@ let testsGlob = rootDir </> "test/**/*.??proj"
 
 let distDir = rootDir </> "dist"
 let distGlob = distDir </> "*.nupkg"
+
+let nugetPublishUrl = "https://www.nuget.org"
+
+
+
+let githubToken = Environment.environVarOrNone "GITHUB_TOKEN"
+Option.iter(TraceSecrets.register "<GITHUB_TOKEN>" ) githubToken
+
+let nugetToken = Environment.environVarOrNone "NUGET_TOKEN"
+Option.iter(TraceSecrets.register "<NUGET_TOKEN>") nugetToken
+
 
 let isRelease (targets : Target list) =
     targets
@@ -68,16 +90,19 @@ Target.create "Test" (fun _ ->
   [sln]
   |> Seq.iter (DotNet.test id)
 )
-
+let releaseNotesFilePath = "RELEASE_NOTES.md"
+let release = lazy(ReleaseNotes.load releaseNotesFilePath)
+let releaseVersion = lazy(release.Value.NugetVersion)
+let releaseNotes = lazy(String.concat "\n" release.Value.Notes)
 // PUBLISH TO NUGET
 
 Target.create "Pack" (fun ctx ->
-    let release = ReleaseNotes.load "RELEASE_NOTES.md"
-    let releaseNotes = (String.concat "\n" release.Notes)
+   
+
     let args =
         [
-            sprintf "/p:PackageVersion=%s" release.NugetVersion
-            sprintf "/p:PackageReleaseNotes=\"%s\"" releaseNotes
+            sprintf "/p:PackageVersion=%s" releaseVersion.Value
+            sprintf "/p:PackageReleaseNotes=\"%s\"" releaseNotes.Value
         ]
     DotNet.pack (fun c ->
         { c with
@@ -90,22 +115,67 @@ Target.create "Pack" (fun ctx ->
 
 
 )
-"Build" ==> "Pack"
 
-Target.create "Publish" (fun _ ->
+Target.create "PublishToNuGet" (fun _ ->
     // target not yet working
     // dotnet nuget push Plough.WebApi.<package>.<version>.nupkg -s https://api.nuget.org/v3/index.json -k <api key>
     Paket.push(fun p ->
         { p with
-            WorkingDir = distDir })
+            ToolType = ToolType.CreateLocalTool()
+            PublishUrl = nugetPublishUrl
+            WorkingDir = distDir
+            ApiKey =
+              match nugetToken with
+              | Some s -> s
+              | _ -> p.ApiKey // assume paket-config was set properly
+        }
+    )
+)
+
+let tagFromVersionNumber versionNumber = sprintf "v%s" versionNumber
+Target.create "GitRelease" (fun _ ->
+    Git.Staging.stageFile "" releaseNotesFilePath
+        |> ignore
+    !! "src/**/AssemblyInfo.fs"
+        |> Seq.iter (Git.Staging.stageFile "" >> ignore)
+
+    Git.Commit.exec "" (sprintf "VER: Bump version to %s\n\n%s" releaseVersion.Value releaseNotes.Value)
+    Git.Branches.push ""
+
+    let tag = tagFromVersionNumber releaseVersion.Value
+
+    Git.Branches.tag "" tag
+    Git.Branches.pushTag "" "origin" tag
+)
+Target.create "GitHubRelease" (fun _ ->
+    let token =
+        match githubToken with
+        | Some s -> s
+        | _ -> failwith "please set the github_token environment variable to a github personal access token with repo access."
+
+    let files = !! distGlob
+
+    GitHub.createClientWithToken token
+    |> GitHub.draftNewRelease gitOwner gitRepoName (tagFromVersionNumber releaseVersion.Value) (release.Value.SemVer.PreRelease <> None) [releaseNotes.Value]
+    |> GitHub.uploadFiles files
+    |> GitHub.publishDraft
+    |> Async.RunSynchronously
 )
 
 Target.create "All" ignore
+Target.create "Release" ignore
 
 "Clean"
   ==> "Restore"
   ==> "Build"
   ==> "Test"
+  ==> "Pack"
   ==> "All"
+
+"Pack"
+  ==> "PublishToNuGet"
+  ==> "GitRelease"
+  ==> "GitHubRelease"
+  ==> "Release"
 
 Target.runOrDefaultWithArguments "All"
